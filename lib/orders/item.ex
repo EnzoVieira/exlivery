153e491354050a8c6ae313a7255487c0f7bf3bdc
Enzo Vieira <enzovieira16@hotmail.com>
2022-11-27 15:25:42 +0000

    Utilizando a lib Decimal

--- lib/orders/item.ex
+++ lib/orders/item.ex
@@ -1,21 +1,29 @@
 defmodule Exlivery.Orders.Item do
   @categories [:pizza, :hamburguer, :carne, :prato_feito, :japonesa, :sobremesa]
 
-  @keys [:description, :category, :unit_price, :quantity]
+  @keys [:description, :category, :unity_price, :quantity]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(description, category, unit_price, quantity)
+  def build(description, category, unity_price, quantity)
       when quantity > 0 and category in @categories do
+    unity_price
+    |> Decimal.cast()
+    |> build_item(description, category, quantity)
+  end
+
+  def build(_, _, _, _), do: {:error, "Invalid parameters!"}
+
+  defp build_item({:ok, unity_price}, description, category, quantity) do
     {:ok,
      %__MODULE__{
        description: description,
        category: category,
-       unit_price: unit_price,
+       unity_price: unity_price,
        quantity: quantity
      }}
   end
 
-  def build(_, _, _, _), do: {:error, "Invalid parameters!"}
+  defp build_item(:error, _, _, _), do: {:error, "Invalid price"}
 end

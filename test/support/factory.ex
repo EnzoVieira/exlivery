7d064a3ee1b69ef453dbafd37fcf8a405e2b26c3
Enzo Vieira <enzovieira16@hotmail.com>
2022-11-27 16:18:56 +0000

    Testando a struct de Item

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,6 +2,7 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
+  alias Exlivery.Orders.Item
 
   def user_factory do
     %User{
@@ -10,6 +11,15 @@ defmodule Exlivery.Factory do
       cpf: "123456",
       age: 20,
       address: "Rua das bananeiras"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      category: :pizza,
+      description: "Pizza de peperoni",
+      quantity: 1,
+      unity_price: Decimal.new("35.5")
     }
   end
 end
